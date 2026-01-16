e9f018ffe6e25422525995cac25bb410f80b618a
Pavel Dovgalyuk <Pavel.Dovgaluk@gmail.com>
2026-01-16 14:07:21 +0300

    Many updates and refactoring

--- labs/export_scheme.sh
+++ labs/export_scheme.sh
@@ -1,2 +1,2 @@
 kicad-cli sch export svg --exclude-drawing-sheet --black-and-white --output schemes $1
-inkscape schemes/$(basename "$1" | cut -d. -f1).svg -d 300 -o schemes/$(basename "$1" | cut -d. -f1).png
+inkscape schemes/$(basename "$1" | cut -d. -f1).svg -d 300 --export-area-drawing -o schemes/$(basename "$1" | cut -d. -f1).png
