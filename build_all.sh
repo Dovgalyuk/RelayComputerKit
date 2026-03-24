38590790cffae2a26f10b26bea06dc1a71116f42
Pavel Dovgalyuk <Pavel.Dovgaluk@gmail.com>
2026-03-24 14:27:05 +0300

    Added more schematics

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,13 @@
+./export_pcb.sh "./modules" "decoder" "./modules-png" "./modules-gerber"
+./export_pcb.sh "./modules" "logic_binary" "./modules-png" "./modules-gerber"
+./export_pcb.sh "./modules" "logic_unary" "./modules-png" "./modules-gerber"
 ./export_pcb.sh "./modules" "register" "./modules-png" "./modules-gerber"
 ./export_pcb.sh "./modules" "switches" "./modules-png" "./modules-gerber"
-./export_pcb.sh "./modules" "logic_unary" "./modules-png" "./modules-gerber"
+./export_scheme.sh "./modules" "decoder" "./modules-png"
+./export_scheme.sh "./modules" "logic_binary" "./modules-png"
+./export_scheme.sh "./modules" "logic_unary" "./modules-png"
 ./export_scheme.sh "./modules" "register" "./modules-png"
 ./export_scheme.sh "./modules" "switches" "./modules-png"
-./export_scheme.sh "./modules" "logic_unary" "./modules-png"
 
 cd labs
 ./build.sh
