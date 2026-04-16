6237b721f787f92ff607a587e6617e1c4fd8a915
Pavel Dovgalyuk <Pavel.Dovgaluk@gmail.com>
2026-04-16 14:27:53 +0300

    Added switchboard module

--- build_all.sh
+++ build_all.sh
@@ -4,6 +4,7 @@
 ./export_pcb.sh "./modules" "logic_unary" "./modules-png" "./modules-gerber"
 ./export_pcb.sh "./modules" "power" "./modules-png" "./modules-gerber"
 ./export_pcb.sh "./modules" "register" "./modules-png" "./modules-gerber"
+./export_pcb.sh "./modules" "switchboard" "./modules-png" "./modules-gerber"
 ./export_pcb.sh "./modules" "switches" "./modules-png" "./modules-gerber"
 ./export_scheme.sh "./modules" "bus" "./modules-png"
 ./export_scheme.sh "./modules" "decoder" "./modules-png"
@@ -11,6 +12,7 @@
 ./export_scheme.sh "./modules" "logic_unary" "./modules-png"
 ./export_scheme.sh "./modules" "power" "./modules-png"
 ./export_scheme.sh "./modules" "register" "./modules-png"
+./export_scheme.sh "./modules" "switchboard" "./modules-png"
 ./export_scheme.sh "./modules" "switches" "./modules-png"
 
 cd labs
