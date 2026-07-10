fee5615fbe4c9e9d0c385182e16a29851d3f08c1
Pavel Dovgalyuk <Pavel.Dovgaluk@gmail.com>
2026-07-10 12:56:48 +0300

    Updated boards

--- build_all.sh
+++ build_all.sh
@@ -4,6 +4,7 @@
 ./export_pcb.sh "./modules" "logic_unary" "./modules-png" "./modules-gerber"
 ./export_pcb.sh "./modules" "power" "./modules-png" "./modules-gerber"
 ./export_pcb.sh "./modules" "register" "./modules-png" "./modules-gerber"
+./export_pcb.sh "./modules" "rom" "./modules-png" "./modules-gerber"
 ./export_pcb.sh "./modules" "switchboard" "./modules-png" "./modules-gerber"
 ./export_pcb.sh "./modules" "switches" "./modules-png" "./modules-gerber"
 ./export_scheme.sh "./modules" "bus" "./modules-png"
@@ -12,6 +13,7 @@
 ./export_scheme.sh "./modules" "logic_unary" "./modules-png"
 ./export_scheme.sh "./modules" "power" "./modules-png"
 ./export_scheme.sh "./modules" "register" "./modules-png"
+./export_scheme.sh "./modules" "rom" "./modules-png"
 ./export_scheme.sh "./modules" "switchboard" "./modules-png"
 ./export_scheme.sh "./modules" "switches" "./modules-png"
 
